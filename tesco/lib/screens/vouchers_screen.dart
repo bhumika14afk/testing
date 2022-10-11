94909f8ecfee0f7bc1257b691abe2dfd2b222550
bhumika <bhumika@celominds.com>
2022-10-11 16:20:30 +0530

    vouchers screen

--- tesco/lib/screens/vouchers_screen.dart
+++ tesco/lib/screens/vouchers_screen.dart
@@ -19,6 +19,111 @@ class _VouchersState extends State<Vouchers> {
         centerTitle: true,
         title: Text("Vouchers"),
       ),
+      body: Column(
+        children: [
+          Card(
+              child: ListTile(
+                  title: Row(
+                    children: [
+                      Text("Total in vouchers",
+                          style: TextStyle(
+                            fontSize: 20,
+                          )),
+                      Padding(
+                        padding: const EdgeInsets.all(15),
+                        child: Text(
+                          "€8",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  trailing: Icon(
+                    Icons.info_outline,
+                    color: Color(0xff00529e),
+                    size: 30,
+                  ))),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Card(
+              color: Colors.grey[400],
+              child: Padding(
+                padding: EdgeInsets.all(15),
+                child: Text(
+                  'Spend your vouchers in-store online and with our reward partners',
+                  style: TextStyle(fontSize: 18),
+                ),
+              ),
+            ),
+          ),
+          Card(
+              elevation: 3,
+              child: ListTile(
+                  shape: Border(top: BorderSide(color: Color(0xff00529e))),
+                  leading: Icon(
+                    Icons.keyboard_arrow_down_sharp,
+                    color: Color(0xff00529e),
+                  ),
+                  title: Text(
+                    "€2.50",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  trailing: Text("Use by 30/11/2022"))),
+          Card(
+              elevation: 3,
+              child: ListTile(
+                  shape: Border(top: BorderSide(color: Color(0xff00529e))),
+                  leading: Icon(
+                    Icons.keyboard_arrow_down_sharp,
+                    color: Color(0xff00529e),
+                  ),
+                  title: Text(
+                    "€3",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  trailing: Text("Use by 28/2/2023"))),
+          Card(
+              elevation: 3,
+              child: ListTile(
+                  shape: Border(top: BorderSide(color: Color(0xff00529e))),
+                  leading: Icon(
+                    Icons.keyboard_arrow_down_sharp,
+                    color: Color(0xff00529e),
+                  ),
+                  title: Text(
+                    "€2.50",
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                  ),
+                  trailing: Text("Use by 31/08/2024"))),
+          Card(
+            elevation: 3,
+            child: ListTile(
+                shape: Border(top: BorderSide(color: Color(0xff00529e))),
+                leading: Icon(
+                  Icons.keyboard_arrow_down_sharp,
+                  color: Color(0xff00529e),
+                ),
+                title: Text(
+                  "€2.50",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                ),
+                trailing: Text("Use by 30/11/2022")),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(15),
+            child: Text(
+              '*View Voucher Terms & Conditions > ',
+              style: TextStyle(
+                  color: Color(0xff00529e),
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
