9abbfc63266e033c8f0e7f2842c8a4c16ff683d5
bhumika <bhumika@celominds.com>
2022-10-13 17:44:54 +0530

    added scroll and options in vouchers page

--- tesco/lib/screens/vouchers_screen.dart
+++ tesco/lib/screens/vouchers_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:linear_progress_bar/ui/dots_indicator.dart';
 import 'NavBar.dart';
 
 class Vouchers extends StatefulWidget {
@@ -19,105 +20,211 @@ class _VouchersState extends State<Vouchers> {
         centerTitle: true,
         title: Text("Vouchers"),
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Card(
-              child: ListTile(
-                  title: Row(
-                    children: [
-                      Text("Total in vouchers",
-                          style: TextStyle(
-                            fontSize: 20,
-                          )),
-                      Padding(
-                        padding: const EdgeInsets.all(15),
-                        child: Text(
-                          "€8",
-                          style: TextStyle(
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      )
-                    ],
+          Column(
+            children: [
+              Card(
+                  child: ListTile(
+                      onTap: () {},
+                      title: Row(
+                        children: [
+                          Text("Total in vouchers",
+                              style: TextStyle(
+                                fontSize: 20,
+                              )),
+                          Padding(
+                            padding: const EdgeInsets.all(15),
+                            child: Text(
+                              "€8",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      trailing: Icon(
+                        Icons.info_outline,
+                        color: Color(0xff00529e),
+                        size: 30,
+                      ))),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: Card(
+                  color: Colors.grey[400],
+                  child: Padding(
+                    padding: EdgeInsets.all(15),
+                    child: Text(
+                      'Spend your vouchers in-store online and with our reward partners',
+                      style: TextStyle(fontSize: 18),
+                    ),
                   ),
-                  trailing: Icon(
-                    Icons.info_outline,
-                    color: Color(0xff00529e),
-                    size: 30,
-                  ))),
-          Padding(
-            padding: const EdgeInsets.all(10),
-            child: Card(
-              color: Colors.grey[400],
-              child: Padding(
-                padding: EdgeInsets.all(15),
-                child: Text(
-                  'Spend your vouchers in-store online and with our reward partners',
-                  style: TextStyle(fontSize: 18),
                 ),
               ),
-            ),
-          ),
-          Card(
-              elevation: 3,
-              child: ExpansionTile(
-                  leading: Icon(
-                    Icons.keyboard_arrow_down_sharp,
-                    color: Color(0xff00529e),
-                  ),
-                  title: Text(
-                    "€2.50",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  trailing: Text("Use by 30/11/2022"))),
-          Card(
-              elevation: 3,
-              child: ExpansionTile(
-                  leading: Icon(
-                    Icons.keyboard_arrow_down_sharp,
-                    color: Color(0xff00529e),
-                  ),
-                  title: Text(
-                    "€3",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  trailing: Text("Use by 28/2/2023"))),
-          Card(
-              elevation: 3,
-              child: ExpansionTile(
-                  leading: Icon(
-                    Icons.keyboard_arrow_down_sharp,
-                    color: Color(0xff00529e),
-                  ),
-                  title: Text(
-                    "€2.50",
-                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                  ),
-                  trailing: Text("Use by 31/08/2024"))),
-          Card(
-            elevation: 3,
-            child: ExpansionTile(
-                leading: Icon(
-                  Icons.keyboard_arrow_down_sharp,
-                  color: Color(0xff00529e),
+              Card(
+                  elevation: 3,
+                  child: ExpansionTile(
+                      children: [
+                        ListTile(onTap: () {}, title: Text('Tap to redeem')),
+                      ],
+                      leading: Icon(
+                        Icons.keyboard_arrow_down_sharp,
+                        color: Color(0xff00529e),
+                      ),
+                      title: Text(
+                        "€2.50",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Text("Use by 30/11/2022"))),
+              Card(
+                  elevation: 3,
+                  child: ExpansionTile(
+                      children: [
+                        ListTile(onTap: () {}, title: Text('Tap to redeem')),
+                      ],
+                      leading: Icon(
+                        Icons.keyboard_arrow_down_sharp,
+                        color: Color(0xff00529e),
+                      ),
+                      title: Text(
+                        "€3",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Text("Use by 28/2/2023"))),
+              Card(
+                  elevation: 3,
+                  child: ExpansionTile(
+                      children: [
+                        ListTile(
+                          title: Text(
+                            'Tap to redeem',
+                          ),
+                          onTap: () {},
+                        ),
+                      ],
+                      leading: Icon(
+                        Icons.keyboard_arrow_down_sharp,
+                        color: Color(0xff00529e),
+                      ),
+                      title: Text(
+                        "€2.50",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Text("Use by 31/08/2024"))),
+              Card(
+                elevation: 3,
+                child: ExpansionTile(
+                    children: [
+                      ListTile(onTap: () {}, title: Text('Tap to redeem')),
+                    ],
+                    leading: Icon(
+                      Icons.keyboard_arrow_down_sharp,
+                      color: Color(0xff00529e),
+                    ),
+                    title: Text(
+                      "€2.50",
+                      style:
+                          TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+                    ),
+                    trailing: Text("Use by 30/11/2022")),
+              ),
+              Card(
+                  elevation: 3,
+                  child: ExpansionTile(
+                      children: [
+                        ListTile(onTap: () {}, title: Text('Tap to redeem')),
+                      ],
+                      leading: Icon(
+                        Icons.keyboard_arrow_down_sharp,
+                        color: Color(0xff00529e),
+                      ),
+                      title: Text(
+                        "€3",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Text("Use by 28/2/2023"))),
+              Card(
+                  elevation: 3,
+                  child: ExpansionTile(
+                      children: [
+                        ListTile(onTap: () {}, title: Text('Tap to redeem')),
+                      ],
+                      leading: Icon(
+                        Icons.keyboard_arrow_down_sharp,
+                        color: Color(0xff00529e),
+                      ),
+                      title: Text(
+                        "€2.90",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Text("Use by 28/2/2023"))),
+              Card(
+                  elevation: 3,
+                  child: ExpansionTile(
+                      children: [
+                        ListTile(onTap: () {}, title: Text('Tap to redeem')),
+                      ],
+                      leading: Icon(
+                        Icons.keyboard_arrow_down_sharp,
+                        color: Color(0xff00529e),
+                      ),
+                      title: Text(
+                        "€2.60",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Text("Use by 28/2/2023"))),
+              Card(
+                  elevation: 3,
+                  child: ExpansionTile(
+                      children: [
+                        ListTile(onTap: () {}, title: Text('Tap to redeem')),
+                      ],
+                      leading: Icon(
+                        Icons.keyboard_arrow_down_sharp,
+                        color: Color(0xff00529e),
+                      ),
+                      title: Text(
+                        "€4",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Text("Use by 28/2/2023"))),
+              Card(
+                  elevation: 3,
+                  child: ExpansionTile(
+                      children: [
+                        ListTile(onTap: () {}, title: Text('Tap to redeem')),
+                      ],
+                      leading: Icon(
+                        Icons.keyboard_arrow_down_sharp,
+                        color: Color(0xff00529e),
+                      ),
+                      title: Text(
+                        "€3",
+                        style: TextStyle(
+                            fontSize: 30, fontWeight: FontWeight.bold),
+                      ),
+                      trailing: Text("Use by 24/2/2023"))),
+              Padding(
+                padding: const EdgeInsets.all(15),
+                child: Text(
+                  '*View Voucher Terms & Conditions > ',
+                  style: TextStyle(
+                      color: Color(0xff00529e),
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold),
                 ),
-                title: Text(
-                  "€2.50",
-                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-                ),
-                trailing: Text("Use by 30/11/2022")),
+              )
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.all(15),
-            child: Text(
-              '*View Voucher Terms & Conditions > ',
-              style: TextStyle(
-                  color: Color(0xff00529e),
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold),
-            ),
-          )
         ],
       ),
     );
